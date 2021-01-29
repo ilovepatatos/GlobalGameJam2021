176829d3c36b68df6153072e7085d14e09ae8a5c
Marc-Antoine Girard <m.antoine.girard@gmail.com>
2021-01-29 02:22:33 -0500

    SaveUtility

---
+++ Assets/Scripts/SaveUtility.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public static class SaveUtility
+{
+    public static void Save<T>(T toSave, string key)
+    {
+        PlayerPrefs.SetString(key, JsonUtility.ToJson(toSave));
+        PlayerPrefs.Save();
+    }
+    public static T Load<T>(string key)
+    {
+        var msg = PlayerPrefs.GetString(key);
+        return (T) JsonUtility.FromJson(msg, typeof(T));
+    }
+}
