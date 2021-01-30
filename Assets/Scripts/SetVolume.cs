0e758bb0dfe6edf8b250bdb33adbeebbc001cc58
Marc-Antoine Girard <m.antoine.girard@gmail.com>
2021-01-29 23:47:58 -0500

    main menu + body update

---
+++ Assets/Scripts/SetVolume.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public string exposedValue;
+    
+    
+    public void SetLevel(float sliderValue)
+    {
+        if(!string.IsNullOrWhiteSpace(exposedValue))
+            mixer.SetFloat("masterVolume", Mathf.Log10(sliderValue) * 20);
+    }
+}
